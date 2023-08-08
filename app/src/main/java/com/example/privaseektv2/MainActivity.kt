f7996b7039dfde3c1e4420e4f405bf7c799d017f
Thomas Dinh <thomas@informatik.hu-berlin.de>
2023-08-08 17:43:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/privaseektv2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.privaseektv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle navigation item clicks here
+            // You can switch fragments or perform actions based on the selected item
+            true
+        }
+    }
+}
